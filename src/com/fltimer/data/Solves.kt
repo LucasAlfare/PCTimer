3f1ec3d7fdad7ae9b2ae22b2167f46d6b61466d6
Lucas Sousa <god-lucas@hotmail.com>
2021-05-25 12:29:59 -0300

    added simple doc

--- src/com/fltimer/data/Solves.kt
+++ src/com/fltimer/data/Solves.kt
@@ -2,6 +2,12 @@ package com.fltimer.data
 
 import java.util.*
 
+/**
+ * Basic extension for a LinkedHashMap.
+ *
+ * This class is used as the main data holder for all objects created over the application
+ * flow.
+ */
 class Solves : LinkedHashMap<UUID, Solve>() {
 
     operator fun plusAssign(solve: Solve) {
